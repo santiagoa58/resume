1fd0efcd01578bfe5754cfe1d3338ffecbed2e15
Santiago Gomez <24705975+santiagoa58@users.noreply.github.com>
2023-05-01 10:16:48 -0400

    [Setup] Lint & prettier setup with pre-push hook (#1)
    * add pre-push git hook to check lint and type errors
    * add type checking to build
    * add prettier and lint rules to pre-push hook
    * add prittierrc config & make it part of eslint

---
+++ client/pre-push.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-console */
+const { exec } = require('child_process');
+
+const commands = ['npm run lint', 'npm run type-check'];
+
+function runCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return reject(error);
+      }
+      if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+      }
+      console.log(`Stdout: ${stdout}`);
+      resolve(stdout);
+    });
+  });
+}
+(async () => {
+  try {
+    for (const command of commands) {
+      console.log(`Running: ${command}`);
+      await runCommand(command);
+    }
+  } catch (error) {
+    console.error('Pre-push check failed. Fix errors before pushing.');
+    process.exit(1);
+  }
+})();

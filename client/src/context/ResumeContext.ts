9afd69d38b0489032088c534b0a4187bf769368d
Santiago Gomez <24705975+santiagoa58@users.noreply.github.com>
2023-06-03 09:48:43 -0400

    fix type error

--- client/src/context/ResumeContext.ts
+++ client/src/context/ResumeContext.ts
@@ -2,6 +2,5 @@ import {createContext, Dispatch} from 'react';
 import {IResume} from '../types/api_types';
 import {ResumeAction} from '../state/resume';
 
-export const ResumeContext = createContext<null| I
->(null);
+export const ResumeContext = createContext<null| IResume>(null);
 export const ResumeDispatchContext = createContext<null | Dispatch<ResumeAction>>(null);

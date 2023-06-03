ed59e801b96f013f666faee11c53af777c506bae
Santiago Gomez <24705975+santiagoa58@users.noreply.github.com>
2023-06-03 09:19:09 -0400

    adds resume context and reducer

--- client/src/context/ResumeContext.ts
+++ client/src/context/ResumeContext.ts
@@ -1,1 +1,7 @@
 import {createContext, Dispatch} from 'react';
+import {IResume} from '../types/api_types';
+import {ResumeAction} from '../state/resume';
+
+export const ResumeContext = createContext<null| I
+>(null);
+export const ResumeDispatchContext = createContext<null | Dispatch<ResumeAction>>(null);
---
+++ client/src/state/resume.ts
@@ -0,0 +1,42 @@
+import {IResume} from '../types/api_types';
+
+type PartialResumeUpdatePayload = Partial<IResume> & Required<Pick<IResume, 'id'>>;
+interface IUpdateResumeAction {
+    type: 'UPDATE_RESUME';
+    // resume update
+    payload: PartialResumeUpdatePayload;
+}
+
+interface IRemoveResumeAction {
+    type: 'REMOVE_RESUME';
+}
+
+interface ISetResumeAction {
+    type: 'SET_RESUME';
+    // resume to set
+    payload: IResume;
+}
+
+export type ResumeAction = IUpdateResumeAction | ISetResumeAction | IRemoveResumeAction;
+
+export const resumeReducer = (
+    state: IResume | undefined,
+    action: ResumeAction
+) => {
+    switch(action.type){
+        case 'REMOVE_RESUME':
+            return undefined;
+        case 'SET_RESUME':
+            return action.payload;
+        case 'UPDATE_RESUME':
+            if(state == undefined){
+                throw new Error('cannot update undefined resume');
+            }
+            if(state.id !== action.payload.id){
+                throw new Error(`resume ID of update doesn't match current state.  did you mean to SET_RESUME?`)
+            }
+            return {...state, ...action.payload}
+        default:
+            return state;
+    }
+};

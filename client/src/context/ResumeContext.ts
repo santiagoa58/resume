4b0e8b641c5bd51e0db1f6d897def4de088a24fd
Santiago Gomez <24705975+santiagoa58@users.noreply.github.com>
2023-06-03 10:09:42 -0400

    adds useSelectedResume hook

--- client/src/context/ResumeContext.ts
+++ client/src/context/ResumeContext.ts
@@ -2,5 +2,5 @@ import {createContext, Dispatch} from 'react';
 import {IResume} from '../types/api_types';
 import {ResumeAction} from '../state/resume';
 
-export const ResumeContext = createContext<null| IResume>(null);
+export const ResumeContext = createContext<null | undefined | IResume>(null);
 export const ResumeDispatchContext = createContext<null | Dispatch<ResumeAction>>(null);
--- client/src/hooks/useResume.ts
+++ client/src/hooks/useResume.ts
@@ -1,4 +1,4 @@
-import {useContext, useCallback, Dispatch} from 'react';
+import {useContext, useCallback, useEffect, Dispatch} from 'react';
 import useAPI from './useAPI';
 import {ResumeContext, ResumeDispatchContext} from '../context/ResumeContext';
 import {IResume} from '../types/api_types';
@@ -30,4 +30,18 @@ export const useGetResume = (): [
     }, [fetchResumeDetails]);
 
     return [resume, getResume];
+};
+
+export const useSelectedResume = (): IResume => {
+    const [resume, getResume] = useGetResume();
+    const [selectedResumeMetadata, _] = useSelectedResumeState();
+
+    // fetch the resume whenever the selected resume is changed
+    useEffect(() => {
+        if(selectedResumeMetadata && resume?.id !== selectedResumeMetadata.id){
+            getResume(selectedResumeMetadata.id);
+        }
+    }, [getResume, selectedResume, resume]);
+
+    return resume;
 };

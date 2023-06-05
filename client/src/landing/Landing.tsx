e56ab21484cf8bcc0dd25951b39027f528d286ef
Santiago Gomez <24705975+santiagoa58@users.noreply.github.com>
2023-06-05 13:47:35 -0400

    adds functionality to action button (#25)
    - removes button with no functionality
    - renames action button to "email me"
    - implements "email me" button functionality

--- client/src/landing/Landing.tsx
+++ client/src/landing/Landing.tsx
@@ -3,26 +3,74 @@ import ResumeSelector from '../resume_selector/ResumeSelector';
 import Title from '../title/Title';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import ArrowForward from '@mui/icons-material/ArrowForwardIos';
 import ContactButtonGroup from '../contact_buttons/ContactButtonGroup';
+import Link from '@mui/material/Link';
+import Skeleton from '@mui/material/Skeleton';
+import { useSelectedResume } from '../hooks/useResume';
+import { IResume } from '../types/api_types';
 
 interface ILandingProps {
   title: string | undefined;
 }
 
-const ActionButtons = () => {
+const getEmailFromResume = (
+  resume: IResume | undefined
+): string | undefined => {
+  return resume?.contacts?.[0] ?? undefined;
+};
+
+const getEmailHrefWithTemplate = (
+  email: string,
+  name?: string,
+  job_title?: string
+) => {
+  const firstName = name?.split(' ')[0];
+  let title = job_title?.toLowerCase() ?? 'software engineer';
+  //capitalize every first letter of title
+  title = title.replace(/\b(\w)/g, (s) => s.toUpperCase());
+  const subject = encodeURIComponent(
+    `Interest in Your ${title ?? 'Software Engineer'} Profile`
+  );
+  const body = encodeURIComponent(
+    `Hello ${
+      firstName ?? 'there'
+    },\n\nI came across your profile and am interested in your skills and experience. Would you be available for a discussion regarding potential opportunities at our company?\n\nKind regards,\n[Your Name]`
+  );
+  return `mailto:${email}?subject=${subject}&body=${body}`;
+};
+
+// link buttons to email
+const ActionButtons: FC = () => {
+  const selectedResume = useSelectedResume();
+  const email = getEmailFromResume(selectedResume);
   return (
     <Box
       sx={{
         display: 'flex',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         gap: '2em',
+        width: '100%',
       }}
     >
-      <Button variant="contained">Contact Me</Button>
-      <Button variant="outlined" endIcon={<ArrowForward />}>
-        Resume
-      </Button>
+      {selectedResume === undefined ? (
+        <Skeleton height="2em" sx={{ width: '50%' }} />
+      ) : (
+        <>
+          {email && (
+            <Link
+              href={getEmailHrefWithTemplate(
+                email,
+                selectedResume?.name,
+                selectedResume?.title
+              )}
+              underline="none"
+            >
+              <Button variant="contained">Email Me</Button>
+            </Link>
+          )}
+          {/* TODO: Download Resume Button */}
+        </>
+      )}
     </Box>
   );
 };

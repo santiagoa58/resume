094187f6ad8434a88b21f61c3d70c58ccc33ac63
Santi G <24705975+santiagoa58@users.noreply.github.com>
2023-06-10 08:38:57 -0400

    implements projects api

--- server/src/projects/handler.py
+++ server/src/projects/handler.py
@@ -1,6 +1,75 @@
 import json
+import os
+import requests
+from typing import List, Dict
+from common.service.manage_secrets import SecretsManagerService
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
+
+ProjectType = Dict[str, any]  # Define this type according to your needs
 
 
-def lambda_handler(event, context):
-    # TODO implement
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+class GithubProjectsReaderService:
+    # constructor
+    def __init__(self):
+        projects_secrets_manager = SecretsManagerService(os.getenv("AWS_REGION"))
+        access_token = projects_secrets_manager.get_secret(
+            os.getenv("GITHUB_ACCESS_TOKEN_SECRET_NAME")
+        )["github_token"]
+        self.auth_headers = {
+            "Authorization": f"token {access_token}",
+            "Accept": "application/vnd.github.v3+json",
+        }
+        self.base_url = os.getenv("GITHUB_BASE_URL")
+        self.username = os.getenv("GITHUB_USERNAME")
+
+    # get a single project's languages from github api
+    def get_github_project_languages(self, project_name: str) -> List[str]:
+        languages_url = (
+            f"{self.base_url}/repos/{self.username}/{project_name}/languages"
+        )
+        languages_response = requests.get(languages_url, headers=self.auth_headers)
+        if languages_response.status_code != 200:
+            raise Exception(languages_response.json())
+        return list(languages_response.json().keys())
+
+    # get github projects from github api
+    def get_github_projects(self) -> List[ProjectType]:
+        repos_url = f"{self.base_url}/users/{self.username}/repos"
+        repos_response = requests.get(repos_url, headers=self.auth_headers)
+        if repos_response.status_code != 200:
+            raise Exception(repos_response.json())
+        repos = repos_response.json()
+        for repo in repos:
+            languages = self.get_github_project_languages(repo["name"])
+            repo["languages"] = languages
+        # Sort repos by updated_at in descending order (most recent first)
+        repos.sort(key=lambda repo: repo["updated_at"], reverse=True)
+
+        return repos
+
+
+def lambda_handler(event, _context):
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    }
+    response = {"statusCode": 200, "headers": headers}
+    if event["httpMethod"] == "GET" and event["path"] == "/projects":
+        service = GithubProjectsReaderService()
+        try:
+            projects = service.get_github_projects()
+            return {
+                "statusCode": 200,
+                "body": json.dumps(projects),
+                "headers": {
+                    "Content-Type": "application/json",
+                },
+            }
+        except Exception as e:
+            return {**response, "statusCode": 500, "body": json.dumps(str(e))}
+    # return error if the request is not a GET projects request
+    return {**response, "statusCode": 400, "body": json.dumps("Bad Request")}
---
+++ server/src/projects/tests/test_handler.py
@@ -0,0 +1,107 @@
+# test project handler
+from projects.handler import lambda_handler
+import json
+
+
+#  get test event
+def _get_test_event(path: str = "projects", pathParameters: str = {}) -> dict:
+    return {
+        "body": "ec4c9d18-bd19-4601-b283-aca637d01b15",
+        "resource": "/{proxy+}",
+        "path": f"/{path}",
+        "httpMethod": "GET",
+        "isBase64Encoded": True,
+        "queryStringParameters": {"foo": "bar"},
+        "multiValueQueryStringParameters": {"foo": ["bar"]},
+        "pathParameters": pathParameters,
+        "stageVariables": {"baz": "qux"},
+        "headers": {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, sdch",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Cache-Control": "max-age=0",
+            "CloudFront-Forwarded-Proto": "https",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "CloudFront-Viewer-Country": "US",
+            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Custom User Agent String",
+            "Via": "1.1 2dc5d267-41c4-4813-8109-9ef0a2a3bb08.cloudfront.net (CloudFront)",
+            "X-Amz-Cf-Id": "7b869b3c-a5f5-4a77-be44-c374c806d5e1==",
+            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+            "X-Forwarded-Port": "443",
+            "X-Forwarded-Proto": "https",
+        },
+        "multiValueHeaders": {
+            "Accept": [
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+            ],
+            "Accept-Encoding": ["gzip, deflate, sdch"],
+            "Accept-Language": ["en-US,en;q=0.8"],
+            "Cache-Control": ["max-age=0"],
+            "CloudFront-Forwarded-Proto": ["https"],
+            "CloudFront-Is-Desktop-Viewer": ["true"],
+            "CloudFront-Is-Mobile-Viewer": ["false"],
+            "CloudFront-Is-SmartTV-Viewer": ["false"],
+            "CloudFront-Is-Tablet-Viewer": ["false"],
+            "CloudFront-Viewer-Country": ["US"],
+            "Host": ["0123456789.execute-api.us-east-1.amazonaws.com"],
+            "Upgrade-Insecure-Requests": ["1"],
+            "User-Agent": ["Custom User Agent String"],
+            "Via": [
+                "1.1 da151c0e-3de4-4686-82e7-7270183a8fdd.cloudfront.net (CloudFront)"
+            ],
+            "X-Amz-Cf-Id": ["82d5026c-a704-4ed4-971e-8feec4bf64c9=="],
+            "X-Forwarded-For": ["127.0.0.1, 127.0.0.2"],
+            "X-Forwarded-Port": ["443"],
+            "X-Forwarded-Proto": ["https"],
+        },
+        "requestContext": {
+            "accountId": "123456789012",
+            "resourceId": "123456",
+            "stage": "prod",
+            "requestId": "28ae6867-5243-4262-b1a9-cef8a952be4f",
+            "requestTime": "09/Apr/2015:12:34:56 +0000",
+            "requestTimeEpoch": 1428582896000,
+            "identity": {
+                "cognitoIdentityPoolId": None,
+                "accountId": None,
+                "cognitoIdentityId": None,
+                "caller": None,
+                "accessKey": None,
+                "sourceIp": "127.0.0.1",
+                "cognitoAuthenticationType": None,
+                "cognitoAuthenticationProvider": None,
+                "userArn": None,
+                "userAgent": "Custom User Agent String",
+                "user": None,
+            },
+            "path": f"/prod/{path}",
+            "resourcePath": "/{proxy+}",
+            "httpMethod": "GET",
+            "apiId": "1234567890",
+            "protocol": "HTTP/1.1",
+        },
+    }
+
+
+def test_lambda_handler_get_all_projects():
+    response = lambda_handler(_get_test_event(), None)
+    assert response["statusCode"] == 200
+    assert response["body"] is not None
+    projects = json.loads(response["body"])
+    assert isinstance(projects, list)
+    assert len(projects) > 0
+    project_name = projects[0].get("name")
+    project_id = projects[0].get("id")
+    assert project_name is not None
+    assert project_id is not None
+    assert isinstance(projects[0]["languages"], list)
+    assert len(projects[0]["languages"]) > 0
+
+
+if __name__ == "__main__":
+    test_lambda_handler_get_all_projects()

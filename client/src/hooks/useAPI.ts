bb1ffef08c2407c62339b1461fdffda773849f9f
Santi G <24705975+santiagoa58@users.noreply.github.com>
2024-11-17 21:21:27 -0800

    feat: implement abort signal for API requests

--- client/src/hooks/useAPI.ts
+++ client/src/hooks/useAPI.ts
@@ -1,36 +1,47 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from 'react';
 import { IProject, IResume, IResumeMetadata } from '../types/api_types';
 
 const RESUMES_ENDPOINT = 'resumes';
 const PROJECTS_ENDPOINT = 'projects';
 
 // base GET request function
-const getRequest = async (url: string) => {
+const getRequest = async (url: string, signal: AbortSignal) => {
   url = encodeURI(url);
-  const response = await fetch(url);
+  const response = await fetch(url, { signal });
   const data = await response.json();
   return data;
 };
 
 // get all resumes
-const getResumes = async (baseURL: string): Promise<IResumeMetadata[]> => {
-  const data = await getRequest(`${baseURL}/${RESUMES_ENDPOINT}`);
+const getResumes = async (
+  signal: AbortSignal,
+  baseURL: string
+): Promise<IResumeMetadata[]> => {
+  const data = await getRequest(`${baseURL}/${RESUMES_ENDPOINT}`, signal);
   return data;
 };
 
 // get all projects
 const getProjects = async (
+  signal: AbortSignal,
   baseURL: string,
   filters?: string
 ): Promise<IProject[]> => {
   filters = filters ? `?filters=${filters}` : '';
-  const data = await getRequest(`${baseURL}/${PROJECTS_ENDPOINT}${filters}`);
+  const data = await getRequest(
+    `${baseURL}/${PROJECTS_ENDPOINT}${filters}`,
+    signal
+  );
   return data;
 };
 
 // get a single resume
-const getResume = async (baseURL: string, id: string): Promise<IResume> => {
-  const data = await getRequest(`${baseURL}/${RESUMES_ENDPOINT}/${id}`);
+const getResume = async (
+  signal: AbortSignal,
+  baseURL: string,
+  id: string
+): Promise<IResume> => {
+  const data = await getRequest(`${baseURL}/${RESUMES_ENDPOINT}/${id}`, signal);
   return data;
 };
 
@@ -47,14 +58,26 @@ const getBaseURL = (): string => {
  */
 export const useAPI = () => {
   const base_api_url = getBaseURL();
+  const abortControllers = useRef(new Map<string, AbortController>());
+  const createAbortController = (key: string) => {
+    if (abortControllers.current.has(key)) {
+      abortControllers.current.get(key)!.abort();
+    }
+    const controller = new AbortController();
+    abortControllers.current.set(key, controller);
+    return controller;
+  };
+
   const fetchAllResumes = useCallback(async () => {
-    const resumes = await getResumes(base_api_url);
+    const controller = createAbortController('fetchAllResumes');
+    const resumes = await getResumes(controller.signal, base_api_url);
     return resumes;
   }, [base_api_url]);
 
   const fetchResumeDetails = useCallback(
     async (id: string) => {
-      const resume = await getResume(base_api_url, id);
+      const controller = createAbortController('fetchResumeDetails');
+      const resume = await getResume(controller.signal, base_api_url, id);
       return resume;
     },
     [base_api_url]
@@ -62,7 +85,12 @@ export const useAPI = () => {
 
   const fetchAllProjects = useCallback(
     async (filters?: string) => {
-      const projects = await getProjects(base_api_url, filters);
+      const controller = createAbortController('fetchAllProjects');
+      const projects = await getProjects(
+        controller.signal,
+        base_api_url,
+        filters
+      );
       return projects;
     },
     [base_api_url]
